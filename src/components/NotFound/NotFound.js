005fb1e209970f6afe60a8b59177a094d7d8b2d8
sabiha-wasema <sabihawasemaa1244@gmail.com>
2022-04-05 22:45:02 +0600

    404page update

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='text-center text-4xl mt-20 font-bold'>
-            <h2>The page is not available.Try Again</h2>
+            <h2>The page is not available.Try Again.</h2>
             <h4><span className='text-red-500'>404</span></h4>
 
         </div>
